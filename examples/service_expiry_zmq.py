0a1b1026b08ee024af6a869736b1579e9e1f42b0
Alex Rudy <alex.rudy@gmail.com>
2015-11-22 15:55:16 -0800

    A test for hanging problems in service expiry.

---
+++ examples/service_expiry_zmq.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Demonstrate REDIS read-write ability.
+"""
+import time
+from lumberjack import setup_logging, setup_warnings_logger
+from logging import getLogger
+
+setup_logging(mode='stream', level=10)
+setup_warnings_logger("")
+log = getLogger("example.zmq")
+
+# Get ready!
+from Cauldron.api import use
+use("zmq")
+from Cauldron import DFW
+
+for i in range(40):
+    log.info("Service Realization {0:d}".format(i))
+    disp = DFW.Service("testsvc", config=None)
+    dtest = disp["TEST"]
+    disp.shutdown()
+
+log.info("Done!")
+log.info("Shutdown complete.")
