fbe01c60bc454139dcb740dced0a22ee5e889ad6
Alex Rudy <alex.rudy@gmail.com>
2015-11-30 14:42:59 -0800

    Fix warning log for KTL XML

--- Cauldron/base/dispatcher.py
+++ Cauldron/base/dispatcher.py
@@ -297,7 +297,7 @@ class Service(object):
                 raise
             warning = CauldronXMLWarning("KTLXML was not loaded correctly. Keywords will not be validated against XML. Exception was {0!s}.".format(e))
             warnings.warn(warning)
-            self.log.warning(warning)
+            self.log.warning("%s", warning)
             self.xml = None
         else:
             # Implementors will be expected to assign Keyword instances
