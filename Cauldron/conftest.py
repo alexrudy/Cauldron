567e0145bacd64bef185b8cfc9eba9e498ffd931
Alex Rudy <alex.rudy@gmail.com>
2016-03-21 15:29:39 -0700

    Fixing ZMQ settings in tests.

--- Cauldron/conftest.py
+++ Cauldron/conftest.py
@@ -109,6 +109,7 @@ def config(tmpdir):
     config = configparser.ConfigParser()
     config.add_section('local')
     config.set('local', 'cache', str(tmpdir))
+    config.add_section('zmq')
     config.set("zmq", "broker", "inproc://broker")
     config.set("zmq", "publish", "inproc://publish")
     config.set("zmq", "subscribe", "inproc://subscribe")
