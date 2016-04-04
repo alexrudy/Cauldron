b45a945e32a6215a5328e92b2e2c9010b127f333
Alex Rudy <alex.rudy@gmail.com>
2016-04-04 16:13:15 -0700

    Bind doesn't resolve hostname unless it is localhost.

--- Cauldron/zmq/common.py
+++ Cauldron/zmq/common.py
@@ -30,7 +30,7 @@ def zmq_get_bind(config, name, group='zmq'):
     if url.startswith("tcp://"):
         parts = list(url.split(":"))
         host = parts[1][2:]
-        if host != "*":
+        if host == "localhost":
             parts[1] = "//" + socket.gethostbyname(host)
         url = ":".join(parts)
     return url
