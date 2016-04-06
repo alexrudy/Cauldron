f12879b854187944f5b6733eae0e602bd09dbb37
Alex Rudy <alex.rudy@gmail.com>
2016-04-06 09:55:48 -0700

    Slightly better thread testing.

--- Cauldron/test_helpers.py
+++ Cauldron/test_helpers.py
@@ -64,7 +64,7 @@ def fail_if_not_teardown():
         raise ValueError("Shouldn't be able to import ktl now!")
     
     # Check for zombie threads.
-    import threading, time
+    import threading, time, gc
     if threading.active_count() > 1:
         time.sleep(0.1) #Allow zombies to die!
     count = 0
@@ -75,6 +75,6 @@ def fail_if_not_teardown():
             
     # If there are new, non-daemon threads, cause an error.
     if count > 1:
-        threads_str = "\n".join([repr(thread) for thread in threading.enumerate()])
+        threads_str = "\n".join(["{0}:{1}".format(repr(thread), gc.get_referrers(thread)) for thread in threading.enumerate()])
         raise ValueError("{0:d} non-deamon thread{1:s} left alive!\n{2!s}".format(
             count-1, "s" if (count-1)>1 else "", threads_str))
