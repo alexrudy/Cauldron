557a124c33662a976f5269e96c018fa75af1346b
Alex Rudy <alex.rudy@gmail.com>
2016-06-02 13:12:11 -0700

    Better GUI example.

--- examples/gui.py
+++ examples/gui.py
@@ -1,36 +1,45 @@
 #!/usr/bin/env python
 
-from Cauldron.api import use, install
+from Cauldron.api import use
 import os, pkg_resources
 os.environ['RELDIR'] = os.path.abspath(pkg_resources.resource_filename("Cauldron", "data/reldir"))
-use("zmq")
-from Cauldron.extern import GUI
+use("local")
 
+from Cauldron import DFW
+from Cauldron.extern import GUI
+SERVICE = 'testsvc'
+
+def setup(service):
+    """Setup the keywords"""
+    DFW.Keyword.types['integer']("TTCAMX", service)
 
 def main():
     """Main function."""
     
-    main_window = MainWindow()
+    dispatcher = DFW.Service(SERVICE, None, setup=setup)
+    main_window = MainWindow(service=SERVICE)
     
     try:
         main_window.run()
     except KeyboardInterrupt:
         pass
-    main_window.shutdown()
-    
+    finally:
+        main_window.shutdown()
+        dispatcher.shutdown()
 
 class MainWindow(GUI.Main.Window):
     def __init__(self, *args, **kwargs):
+        service = kwargs.pop("service")
         GUI.Main.Window.__init__(self, *args, **kwargs)
         self.title ('Test Cauldron GUI')
-        self.initializeService ('testsvc')
+        self.initializeService(service)
         area = self.area = GUI.Box.Color(self, background=GUI.Color.grey)
         self.ttcamx = InfoBox(area, 'TT CAM X')
         self.ttcamx.value.setKeyword(self.service['ttcamx'])
     
 class InfoBox (GUI.Box.Info):
 
-    def __init__ (self, colorbox, label, value=GUI.Value.Entry, button=GUI.Button.Status):
+    def __init__ (self, colorbox, label, value=GUI.Value.Entry, button=None):
 
         GUI.Box.Info.__init__ (self, colorbox, label, value, button)
 
