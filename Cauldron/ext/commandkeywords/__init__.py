93474c192516864b2c609f2225a0f6c1fa8ca9a8
Alex Rudy <alex.rudy@gmail.com>
2016-11-07 16:51:57 -0800

    Make command-keyword compatible with DFW implementation

--- Cauldron/ext/commandkeywords/__init__.py
+++ Cauldron/ext/commandkeywords/__init__.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import
 
 from Cauldron.types import Boolean, DispatcherKeywordType
 from Cauldron.exc import NoWriteNecessary
+from Cauldron.utils.callbacks import Callbacks
 
 class CommandKeyword(Boolean, DispatcherKeywordType):
     """This keyword will receive boolean writes as 1, and will always be set to 0. 
@@ -20,12 +21,22 @@ class CommandKeyword(Boolean, DispatcherKeywordType):
     def __init__(self, *args, **kwargs):
         kwargs['initial'] = '0'
         super(CommandKeyword, self).__init__(*args, **kwargs)
+        self._cbs = Callbacks()
+    
+    def command(self, func):
+        """Add command items."""
+        self._cbs.add(func)
     
     def prewrite(self, value):
         """Before writing, trigger no-write-necssary if value is False"""
         if self.translate(value) == '0':
             raise NoWriteNecessary("No write needed, command not triggered.")
         return super(CommandKeyword, self).prewrite(value)
+    
+    def write(self, value):
+        """Write to the commands."""
+        if str(value) == '1':
+            self._cbs(self)
     
     def postwrite(self, value):
         """Special postwrite that always sets the value to '0'."""
