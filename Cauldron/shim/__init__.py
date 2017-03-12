453f4f48dad0028f3ceec435470d19a574c7419a
Alex Rudy <alex.rudy@gmail.com>
2017-03-11 21:22:56 -0800

    Support ktl classes using a shim

---
+++ Cauldron/shim/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+"""
+Provides a compatibility shim for ktl/DFW services.
+"""
---
+++ Cauldron/shim/dispatcher.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# from ..base import DispatcherService, DispatcherKeyword
+
+class Service(object):
+    """Handle abstract methods from base dispatcher."""
+    pass
+    
+class Keyword(object):
+    """Handle abstract methods from base dispatcher """
+    pass
+    
