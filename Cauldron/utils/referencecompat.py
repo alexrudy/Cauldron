84ff88d08a12f979b6a88b2ef8caa0eb115f0bba
Alex Rudy <alex.rudy@gmail.com>
2017-03-16 00:19:18 -0700

    Remove a six.moves.builtins reference

--- Cauldron/utils/referencecompat.py
+++ Cauldron/utils/referencecompat.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 try:
-    from six.moves.builtins import ReferenceError
+    from __builtins__ import ReferenceError
 except (NameError, ImportError): # pragma: no cover
     from weakref import ReferenceError
 
