8ad9f3cf7e2e5cdbdb54c508e161b66ec8cf3fc9
Alex Rudy <alex.rudy@gmail.com>
2017-03-16 00:18:55 -0700

    Add a ktl_context extension
    Really useful for scripting KTL things.

--- Cauldron/ext/attr/__init__.py
+++ Cauldron/ext/attr/__init__.py
@@ -2,10 +2,51 @@
 Expose keywords as attributes on ktl services.
 """
 
+import contextlib
+
 def get_service(name):
     """Generate a service object for a given name."""
     from Cauldron.ktl.procedural import cached
-    return cached(name)
+    if isinstance(name, (str, bytes))
+        return cached(name)
+    return name
+    
+__all__ = ['Service', 'ktl_context']
+
+@contextlib.contextmanager
+def ktl_context(service, **kwargs):
+    """A context in which various keyword values are set to the desired value.
+    
+    Use as follows::
+        
+        with ktl_context("saocon2", loop="open"):
+            # Do some work here with loop = 'open'
+            print("The loop is open!")
+        
+    This is equivalent, roughly, to::
+        
+        service = ktl.Service("saocon2")
+        loop = service['loop'].read()
+        try:
+            service['loop'].write('open')
+            # Do some work here with loop = 'open'
+            print("The loop is open!")
+        finally:
+            service['loop'].write(loop)
+    
+    """
+    service = get_service(service)
+    initial_state = {}
+    # Record the initial state.
+    for keyword in kwargs:
+        initial_state[keyword] = service[keyword].read()
+    try:
+        for keyword, value in kwargs.items():
+            service[keyword].write(value)
+        yield
+    finally:
+        for keyword, value in initial_state:
+            service[keyword].write(value)
 
 class Service(object):
     """An attribute service"""
@@ -15,10 +56,7 @@ class Service(object):
     
     def __init__(self, name_or_service):
         super(Service, self).__init__()
-        if isinstance(name_or_service, (str, bytes)):
-            self._service = get_service(name_or_service)
-        else:
-            self._service = name_or_service
+        self._service = get_service(name_or_service)
         
     def __getattr__(self, name):
         """Get an attribute."""
