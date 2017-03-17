2c37e174dab6ee6b5e200d88a4bc3913995c3ee0
Alex Rudy <alex.rudy@gmail.com>
2017-03-16 19:23:09 -0700

    Improvements to the enumeration script

--- examples/enumeration.py
+++ examples/enumeration.py
@@ -4,6 +4,7 @@
 import threading
 import click
 import contextlib
+import logging
 
 def setup(service):
     """Set up the KTL service with some dummy keywords."""
@@ -44,17 +45,55 @@ def client(service, keyword, n):
     finally:
         kw.write(initial, wait=False)
     
+@contextlib.contextmanager
+def broker():
+    """Broker runner."""
+    from Cauldron import registry
+    from Cauldron.zmq.broker import ZMQBroker
+    if registry.dispatcher.backend == "zmq":
+        b = ZMQBroker.thread()
+    try:
+        yield
+    finally:
+        if registry.dispatcher.backend == "zmq":
+            b.stop()
+    
+def configure():
+    """Configure Cauldron"""
+    from Cauldron.config import cauldron_configuration
+    cauldron_configuration.set("zmq", "broker", "inproc://broker")
+    cauldron_configuration.set("zmq", "publish", "inproc://publish")
+    cauldron_configuration.set("zmq", "subscribe", "inproc://subscribe")
+    cauldron_configuration.set("zmq", "pool", "2")
+    cauldron_configuration.set("zmq", "timeout", "5")
+    cauldron_configuration.set("core", "timeout", "5")
+    
+def logging_level(f):
+    """Logging arguments."""
+    f = click.option("--verbose", "level", flag_value=logging.NOTSET, help="Use verbose logging.")(f)
+    f = click.option("--debug", "level", flag_value=logging.DEBUG, help="Use debug logging.")(f)
+    f = click.option("--info", "level", default=True, flag_value=logging.INFO, help="Default logging.")(f)
+    f = click.option("--quiet", "level", flag_value=logging.ERROR, help="Make things quiet.")(f)
+    return f
+    
 @click.command()
 @click.option("--dispatcher/--no-dispatcher", default=False, help="Run a dispatcher.")
 @click.option("-k", "--backend", default='local', help='The Cauldron backend to select.')
 @click.option("-s", "--service", default='saotest', help="Service to test against.")
 @click.option("--keyword", type=str, default="WFSCENTROID", help="Keyword to examine.")
 @click.option("-n", type=int, default=3, help="Number of enumerations to examine.")
-def main(keyword, n, dispatcher, backend, service):
+@logging_level
+def main(keyword, n, dispatcher, backend, service, level):
     """Test against an enumerated keyword."""
     import Cauldron
+    import lumberjack
+    lumberjack.setup_logging(mode='stream', level=level)
+    log = logging.getLogger()
+    log.setLevel(level)
+    
+    configure()
     Cauldron.use(backend)
-    with dispatch(dispatcher, service):
+    with broker(), dispatch(dispatcher, service):
         client(service, keyword, n)
     
 if __name__ == '__main__':
