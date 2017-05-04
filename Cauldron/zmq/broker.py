0ee8147b768567658a9388d1092df27887f045ef
Alex Rudy <alex.rudy@gmail.com>
2017-05-03 17:36:07 -0700

    Minor fix in broker KeyError

--- Cauldron/zmq/broker.py
+++ Cauldron/zmq/broker.py
@@ -401,7 +401,7 @@ class Service(object):
                 dispatcher = self.dispatchers[dispatcher_name]
                 ktl_type = dispatcher.keywords[message.keyword.upper()]
             else:
-                raise KeyError
+                raise KeyError(message.keyword.upper())
         except KeyError:
             fmessage = FanMessage(self, client, message)
             self._fans[fmessage.id] = fmessage
